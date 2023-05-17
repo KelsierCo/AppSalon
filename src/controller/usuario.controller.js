c982b5bdf79d76cc61c8cd62beae1d4344ee4ffc
Carlos Moreno <caralfmo@gmail.com>
2023-05-16 22:04:19 -0500

    funciones CRUD para usuario terminado

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -38,26 +38,37 @@ const crearUsuario = async (req, res) => {
 };
 
 const eliminarUsuario = async (req, res) => {
-    const { id } = req.body;
+  const { id } = req.body;
 
-    const result = await pool.query("DELETE from usuario WHERE id = $1", [id]);
+  const result = await pool.query("DELETE from usuario WHERE id = $1", [id]);
 
-    res.json("concuerdo")
-}
+  if (result.rows.length === 0)
+    return res.status(404).json({
+      message: "usuario no encontrado",
+    });
+  
+    return res.sendStatus(204);
+};
 
 const cambiarContrasena = async (req, res) => {
-    const { id } = req.params
-    const { contrasena } = req.body
+  const { id } = req.params;
+  const { contrasena } = req.body;
 
-    const result = await pool.query("UPDATE usuario SET contrasena = $1 WHERE id = $2", [contrasena, id])
-    
-    res,json("actualizado")
-}
+  const result = await pool.query(
+    "UPDATE usuario SET contrasena = $1 WHERE id = $2 RETURNING *",
+    [contrasena, id]
+  );
+  if (result.rows.length === 0)
+  return res.status(404).json({
+    message: "usuario no encontrado",
+  });
+  res.json(result.rows[0]);
+};
 
 module.exports = {
   getUsuarios: getUsuarios,
   getUsuario: getUsuario,
   crearUsuario: crearUsuario,
   eliminarUsuario: eliminarUsuario,
-  cambiarContrasena: cambiarContrasena
+  cambiarContrasena: cambiarContrasena,
 };
