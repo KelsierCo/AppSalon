d135f42fd5159242d856590e7ab6830ace9531de
Carlos Moreno <caralfmo@gmail.com>
2023-06-02 17:46:34 -0500

    agregar calendario en eventos

--- client/src/components/general/Eventos.jsx
+++ client/src/components/general/Eventos.jsx
@@ -1,6 +1,20 @@
+import { useState } from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+
 function Eventos() {
+  const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio"]
+  const [Dia, setDia] = useState(new Date());
+  console.log(Dia.getMonth())
   return (
-    <div>eventos</div>
+    <div>
+      <div>
+        <h3>Eventos del dia {Dia.getDate()} de {meses[Dia.getMonth()]} del {Dia.getFullYear()}</h3>      
+      </div>
+      <Calendar onChange={setDia} value={Dia}/>
+
+    </div>
+    
   )
 }
 
